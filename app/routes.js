40d2115696134fb788e64a50aa3e0759257e4484
Renato Mintz <remintz@gmail.com>
2022-05-25 18:36:23 -0300

    Commit pendente há séculos

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,8 @@
 // here we define all the server's routes (GET, POST, PUT or DELETE)
 
 // modules =================================================
+var sys = require('sys');
+var exec = require('child_process').exec;
 var config = require('./config/config');
 var omxplayer = require('node-omxplayer');
 var videoPlayer;
@@ -15,51 +17,68 @@ module.exports = function (app) {
     app.post('/api/play-pause', playOmx);
     // stops a video on the raspberry pi
     app.post('/api/stop',  stopOmx);
+    // reboot the machine
+    app.post('/api/reboot', reboot);
+    // sync time 
+    app.post('/api/time', syncTime);
 
     // functions ===============================================================
     function startOmx (req, res) {
+        if (videoPlayer){
+            res.json({ 'success': false, 'playing': true, 'delay': 0 });
+            return;
+        } 
         var videoPath = '/home/pi/videos/' + config.piNumber + '.mp4';
+        var date_ob = new Date();
+        var delay = (60 - date_ob.getSeconds())*1000;
         videoPlayer = omxplayer(videoPath, 'hdmi', true);
+        videoPlayer.pause();
 
-        console.log(videoPath);
-        res.json({ 'success': true, 'playing': true });
+        setTimeout(() => videoPlayer.play(), delay);
+
+        res.json({ 'success': true, 'playing': true, 'delay': delay/1000 });
+        console.log("start");
+    }
+
+     // time
+     function syncTime (req, res) {
+        exec("sudo /home/pi/relogio.sh", puts);  
+        res.json({ 'success': true, 'playing': false });
+        console.log("sync");
     }
 
     function playOmx (req, res) {
         if (videoPlayer){
             videoPlayer.play();
             res.json({'success': true, 'playing': true})
+            console.log("play");
         }else{
             res.status(404);
             res.json({'success': false, 'playing': false})
+            console.log("play");
         }
     }
 
     function stopOmx (req, res) {
         if (videoPlayer){
             videoPlayer.quit();
+            videoPlayer = null;
             res.json({'success': true, 'playing': false})
+            console.log("stop");
         }else{
             res.status(404);
             res.json({'success': false, 'playing': false})
+            console.log("stop");
         }
     }
 
+    function puts (error, stdout, stderr) { sys.puts(stdout)}
 
-    // // plays a video on the raspberry pi
-    // function playVideo(req, res) {
-    //     // checks if params were passed
-    //     if (!req.body.video) {
-    //         // no video to play, return error
-    //         res.status(404);
-    //         res.send('Please provide a video to play on the Pi');
-    //     } else {
-    //         // there is a video to play, keep going
-    //         var videoPath = '/home/pi/videos/' + req.body.video
+    // reboots
+    function reboot (req, res) {
+        exec("sudo reboot", puts);    
+    }
 
-    //         var videoPlayer = omxplayer(videoPath);
 
-    //         res.json({ 'success': true, 'playing': true });
-    //     }
-    // }
 };
+
