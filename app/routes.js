3562e7f18782d8ae206c57f72a53b61dfd5202ea
Renato Mintz <remintz@gmail.com>
2022-05-26 08:12:52 -0300

    testando melhorias

--- app/routes.js
+++ app/routes.js
@@ -7,14 +7,27 @@ var exec = require('child_process').exec;
 var config = require('./config/config');
 var omxplayer = require('node-omxplayer');
 var videoPlayer;
+var nowPlaying = false;
+
+
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) {
+    var timestamp = new Date().toISOString();
+    log_file.write(timestamp + ' - ' + util.format(d) + '\n');
+    log_stdout.write(util.format(d) + '\n');
+};
 
 // configuration ===========================================
 module.exports = function (app) {
     // server routes ===========================================================
     // starts a video on the raspberry pi
     app.post('/api/start', startOmx);
-    // plays a video on the raspberry pi
-    app.post('/api/play-pause', playOmx);
+    // plays/pauses a video on the raspberry pi
+    app.post('/api/play-pause', playPauseOmx);
     // stops a video on the raspberry pi
     app.post('/api/stop',  stopOmx);
     // reboot the machine
@@ -22,22 +35,29 @@ module.exports = function (app) {
     // sync time 
     app.post('/api/time', syncTime);
 
+    function setupVideo() {
+        var videoPath = '/home/pi/videos/' + config.piNumber + '.mp4';
+        var videoPlayer = omxplayer(videoPath, 'hdmi', true);
+        return videoPlayer
+    }
+
     // functions ===============================================================
     function startOmx (req, res) {
         if (videoPlayer){
             res.json({ 'success': false, 'playing': true, 'delay': 0 });
             return;
         } 
-        var videoPath = '/home/pi/videos/' + config.piNumber + '.mp4';
-        var date_ob = new Date();
-        var delay = (60 - date_ob.getSeconds())*1000;
-        videoPlayer = omxplayer(videoPath, 'hdmi', true);
+        videoPlayer = setupVideo();
         videoPlayer.pause();
 
-        setTimeout(() => videoPlayer.play(), delay);
+        var delay = 5000;
+        setTimeout(() => {
+            videoPlayer.play();
+            nowPlaying = true;
+        }, delay);
 
         res.json({ 'success': true, 'playing': true, 'delay': delay/1000 });
-        console.log("start");
+        console.log("start with delay");
     }
 
      // time
@@ -47,14 +67,21 @@ module.exports = function (app) {
         console.log("sync");
     }
 
-    function playOmx (req, res) {
-        if (videoPlayer){
+    function playPauseOmx (req, res) {
+        if (! videoPlayer) {
+            videoPlayer = setupVideo();
+            nowPlaying = false;
+        }
+        if (nowPlaying) {
+            videoPlayer.pause();
+            nowPlaying = false;
+            res.json({'success': true, 'playing': false})
+            console.log("pause");
+            }
+        else {
             videoPlayer.play();
+            nowPlaying = true;
             res.json({'success': true, 'playing': true})
-            console.log("play");
-        }else{
-            res.status(404);
-            res.json({'success': false, 'playing': false})
             console.log("play");
         }
     }
@@ -63,6 +90,7 @@ module.exports = function (app) {
         if (videoPlayer){
             videoPlayer.quit();
             videoPlayer = null;
+            nowPlaying = false;
             res.json({'success': true, 'playing': false})
             console.log("stop");
         }else{
